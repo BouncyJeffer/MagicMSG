7d5519841f771eb7f35e7faa5409d0b08278394d
BouncyJeffer <BouncyJeffer@gmail.com>
2017-04-23 11:06:59 -0700

    Changed sub-commands to give more simplicity & added popups.
    The old sub-command "sendMsg" was changed to "msg" and added the ability to send popups by entering
    /magicmsg popup <player|all> <message>

--- src/MagicMSG/main.php
+++ src/MagicMSG/main.php
@@ -19,21 +19,38 @@ class main extends PluginBase {
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
    	if(strtolower($command->getName()) == "magicmw"){
 		switch(strtolower($args[0])){
-			case "sendmsg":
+			case "msg":
 			if(strtolower($arg[1]) == "all"){
 				$players = $this->getServer()->getOnlinePlayers();
 				$sender->sendMessage(TextFormat::GREEN."Sending...");
 				foreach($players as $p){
-					$p->sendMessage(substr(implode(" ", $args), strlen($args[1]) + 3));	
+					$p->sendMessage(substr(implode(" ", $args), 6));	
 				}
 				die;
 			}
 			$player = $this->getServer()->getPlayer($args[0]);
 			if($player instanceof Player){
-				$player->sendMessage(substr(implode(" ", $args), strlen($args[1]) + 7));
+				$player->sendMessage(substr(implode(" ", $args), strlen($args[1]) + 3));
 				$sender->sendMessage(TextFormat::GREEN."Message sent!");
 			} else {
 				$sender->sendMessage(TextFormat::RED."Error: Player not found.");
+			}
+			break;
+			case "popup":
+			if(strtolower($arg[1]) == "all"){
+				$players = $this->getServer()->getOnlinePlayers();
+				$sender->sendMessage(TextFormat::GREEN."Sending...");
+				foreach($players as $p){
+					$p->sendPopup(substr(implode(" ", $args), 8));	
+				}
+				die;
+			}
+			$player = $this->getServer()->getPlayer($args[0]);
+			if($player instanceof Player){
+				$player->sendPopup(substr(implode(" ", $args), strlen($args[1]) + 5));
+				$sender->sendMessage(TextFormat::GREEN."Popup sent!");
+			} else {
+				$sender->sendMessage(TextFormat::RED."Error: Player not found.");	
 			}
 			break;
 		}
